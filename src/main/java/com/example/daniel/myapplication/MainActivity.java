4e85db771bec146a3dee2606d3ad13ad9ddf4a48
Daniel Nguyen <danielhuannguyen@gmail.com>
2015-03-04 18:41:32 -0600

    Initial Commit

---
+++ src/main/java/com/example/daniel/myapplication/MainActivity.java
@@ -0,0 +1,103 @@
+//Daniel Nguyen
+//This is my first android app. This is a basic quote application.
+
+package com.example.daniel.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RelativeLayout touch = (RelativeLayout) findViewById(R.id.touch);
+        final TextView quoteText = (TextView) findViewById(R.id.quote);
+        final TextView personText = (TextView) findViewById(R.id.person);
+
+        final ArrayList<Quote> quoteList = new ArrayList<Quote>();
+
+        Quote quote4 = new Quote("You're more of a fun vampire. You don't suck blood, you just suck.", "Troy Barnes");
+        quoteList.add(quote4);
+
+        Quote quote1 = new Quote("Cool Beans", "Rod Kimble");
+        quoteList.add(quote1);
+
+        Quote quote2 = new Quote("How can mirrors be real if our eyes aren't real", "Jaden Smith");
+        quoteList.add(quote2);
+
+        Quote quote3 = new Quote("That's like me blaming owls for how bad I suck at analogies.", "Britta Perry");
+        quoteList.add(quote3);
+
+        Quote quote5 = new Quote("I was gonna be the first person in my family to graduate from community college. Everyone else graduated from normal college", "Troy Barnes");
+        quoteList.add(quote5);
+
+        Quote quote6 = new Quote ("Time is just wibelly wobbley timey wimey... stuff", "The Doctor");
+        quoteList.add(quote6);
+        //Add more quotes here
+
+
+        final int[] count = {0};
+        touch.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+                if (count[0] < quoteList.size()) {
+
+                    Quote q = quoteList.get(count[0]);
+
+                    quoteText.setText(q.getQuote());
+
+                    personText.setText(q.getPerson());
+
+                    count[0] = count[0] + 1;
+
+
+                } else{
+
+                    count[0] = 0;
+
+                }
+
+
+
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
